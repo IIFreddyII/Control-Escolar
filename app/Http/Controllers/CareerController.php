6b0e5608e55d1dde5efff77766c9ebf6e4e529cd
unknown <iifreddyii>
2022-04-14 01:46:41 -0500

    Modelos y Migraciones de las tablas Materias, Departamentos, Carreras, Alumnos, Catedraticos, Grupos

---
+++ app/Http/Controllers/CareerController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Career;
+use Illuminate\Http\Request;
+
+class CareerController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    
+    public function create()
+    {
+        //
+    }
+
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    
+    public function show(Career $career)
+    {
+        //
+    }
+
+    
+    public function edit(Career $career)
+    {
+        //
+    }
+
+    
+    public function update(Request $request, Career $career)
+    {
+        //
+    }
+
+    
+    public function destroy(Career $career)
+    {
+        //
+    }
+}
