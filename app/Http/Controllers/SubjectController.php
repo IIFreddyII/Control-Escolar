62a7e199a0783c3370ba3da1526e8e7b728477ea
unknown <iifreddyii>
2022-04-16 18:08:14 -0500

    Se agrego el Show, insert, update y delete en el controlador y las rutas en el archivo web

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use App\Models\Subject;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 
 class SubjectController extends Controller
@@ -23,13 +25,30 @@ class SubjectController extends Controller
     
     public function store(Request $request)
     {
-        //
+        //$subject = new Subject();
+        /*validacion de los campos*/
+        $validator = Validator::make($request->all(), [
+            'nSub' => 'required|max:3|unique:subjects,nSub',
+            'name' => 'required|max:20|unique:subjects,name',
+        ]);
+
+        if($validator->fails()){
+            return $validator->errors();
+        }
+
+        $subject = Subject::create([
+            'nSub' => $request->nSub,
+            'name' => $request->name,
+        ]);
+        echo $request->name;
     }
 
     
-    public function show(Subject $subject)
+
+    public function show($id)
     {
-        //
+        $subject = Subject::find($id);
+        return $subject;
     }
 
     
@@ -39,14 +58,38 @@ class SubjectController extends Controller
     }
 
     
-    public function update(Request $request, Subject $subject)
+    public function update(Request $request)
     {
-        //
+        /*validacion de los campos*/
+        $validator = Validator::make($request->all(), [
+            'nSub' => 'required|max:3|unique:subjects,nSub',
+            'name' => 'required|max:20|unique:subjects,name',
+        ]);
+
+        if($validator->fails()){
+            return $validator->errors();
+        }
+        
+        $subject = Subject::find($request->id);
+        $subject->nSub = $request->nSub;
+        $subject->name = $request->name;
+
+        /**Actualizar la informacion */
+        $subject->save();
+        return $subject;
     }
 
     
-    public function destroy(Subject $subject)
+    public function destroy(Requets $request)
     {
-        //
+        $subject = Subject::find($request->id);
+        $subject->delete();
+        $subject = Subject::all();
+        return $subject;
+    }
+
+    //generar un token
+    public function create_token(){
+        return csrf_token();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,16 @@ use App\Http\Controllers\SubjectController;
 Route::get('/', function () {
     return view('welcome');
 });
+/**Gerarr token */
+Route::get('/create_token', [SubjectController::class, 'create_token']);
 
 /*Ruta para ver todos los datos de todas las materias*/
 Route::get('/subjects_index', [SubjectController::class, 'index']);
+/*Ruta Insertar datos */
+Route::post('/subject_insert', [SubjectController::class, 'store']);
+/*Ruta para ver datos por ID*/
+Route::get('/subject_show/{id}', [SubjectController::class, 'show']);
+/*Ruta para ver todos los datos de todas las materias*/
+Route::put('/subject_update', [SubjectController::class, 'update']);
+/*Ruta para eliminar registros*/
+Route::post('/subject_delete', [SubjectController::class, 'destroy']);
